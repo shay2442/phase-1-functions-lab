b920b7834f8a375f33f957124d00c40e1476f343
Shay Wacker <shay2442@yahoo.com>
2021-08-11 10:55:10 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    if (street > 42) {
+        return street - 42
+    }
+    else {
+        return 42 - street      
+    }
+}
+  
+function distanceFromHqInFeet(street) {
+
+
+        return distanceFromHqInBlocks(street) * 264
+
+    }
+
+function distanceTravelledInFeet(street, streetTwo) {
+    if (street > 42) {
+        return (streetTwo - street) * 264
+    }
+    else {
+        return (street - streetTwo) * 264
+
+    }
+}
+
+function calculatesFarePrice(street, streetTwo) {
+    const feet = distanceTravelledInFeet(street, streetTwo);
+
+    if (feet < 400) {
+        return 0;
+    }
+
+    else if (feet > 400 && feet < 2000) {
+        return (feet - 400) * .02;
+    }
+    
+    else if (feet > 2000 && feet < 2500) {
+        return 25;
+    }
+
+    else if (feet > 2500) {
+        return 'cannot travel that far'
+    }
+
+}
